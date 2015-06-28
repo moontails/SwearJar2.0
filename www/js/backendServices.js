7581c980cb855b03272afd417ac9af573106e988
Pranav Moktali <pmoktali@enova.com>
2015-06-28 05:13:16 -0500

    firebase integration

--- www/js/backendServices.js
+++ www/js/backendServices.js
@@ -1,10 +1,11 @@
 angular.module('starter.backendServices', [])
 
 .factory("Quests", function($firebaseArray) {
+
+
   var itemsRef = new Firebase("https://sizzling-fire-9071.firebaseio.com/quests");
   return $firebaseArray(itemsRef);
 })
-
 
 .factory("Users", ['$q', function($q){
   var Users = {};
@@ -58,24 +59,3 @@ angular.module('starter.backendServices', [])
     return Users;
 
   }]);
-// .factory('Firebase', function(){
-//   var Firebase = {};
-//
-//   Firebase.getUserDetails = function(username, password) {
-//     // Get user from mogo db.
-//
-//     var userDetails = {
-//       name: "Pranav Moktali",
-//       email: "pranav.moktali@gmail.com",
-//       phone: "1232122",
-//       username: "mokpro"
-//     };
-//
-//     return userDetails;
-//   };
-//
-//   Firebase.getQuests = function(username) {
-//     // get quests from browser
-//   }
-//   return Firebase;
-// });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,15 +24,21 @@ angular.module('starter.controllers', [])
   };
 
   $scope.setUser = function () {
+<<<<<<< HEAD
 
   };
 
+=======
+    Firebase.getUserDetails()
+  };
+>>>>>>> firebase integration
 }])
 
 .controller('QuestCtrl',['$scope', '$rootScope', '$state', 'Quests', function($scope, $rootScope, $state, Quests) {
   $scope.data = {};
   $scope.quests = Quests;
   $scope.addQuest = function() {
+<<<<<<< HEAD
     $scope.data.count = 0;
     var now = new Date();
     now.setDate(now.getDate() + parseInt($scope.data.duration));
@@ -45,6 +51,17 @@ angular.module('starter.controllers', [])
 
   $scope.selectCharity = function() {
     $state.go('tab.search-charity');
+=======
+    console.log(JSON.stringify($scope.data));
+    $scope.quests.$add($scope.data);
+  };
+
+
+  //$scope.oncreate = QuestService.save($scope.data);
+<<<<<<< HEAD
+  $scope.oncreate = function() {
+    QuestService.save($scope.data);
+>>>>>>> firebase integration
   };
   
 
@@ -66,6 +83,7 @@ angular.module('starter.controllers', [])
       });
       console.log("Received results "+ $scope.results.length);
   };
+<<<<<<< HEAD
 
   $scope.onselect = function(name) {
     console.log("Selected Charity",name);
@@ -82,6 +100,17 @@ angular.module('starter.controllers', [])
   };
 
 }])
+=======
+})
+=======
+  // $scope.oncreate = function() {
+  //   QuestService.save($scope.data);
+  // };
+  // $scope.quests = QuestService.all();
+
+}])
+>>>>>>> firebase integration
+>>>>>>> firebase integration
 
 .controller('DashCtrl', function($scope) {})
 
@@ -104,6 +133,10 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> firebase integration
 .controller('AccountCtrl', function($scope) {
 
 });
