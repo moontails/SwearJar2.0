f22498989089961b2567e93793c8c1547f00b941
Vijetha Vijayendran <vjyndrn2@illinois.edu>
2015-06-27 22:55:11 -0500

    Create quest functionality

--- www/js/quest_controller.js
+++ www/js/quest_controller.js
@@ -4,6 +4,7 @@ angular.module('starter.quest', [])
 
   return {
       save: function (data) {
+        console.log("Saved quest data for " + data.name);
         allQuests.push(data);
       }
   };
