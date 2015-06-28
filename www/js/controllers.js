65f7555314e2a021ebb3d0d810e81c4a53edd49c
Pranav Moktali <pmoktali@enova.com>
2015-06-28 06:26:30 -0500

    user authentication completed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,6 +23,13 @@ angular.module('starter.controllers', [])
     Users.createUser($scope.data.username, $scope.data.password);
   };
 
+<<<<<<< HEAD
+=======
+  $scope.setUser = function () {
+
+  };
+
+>>>>>>> user authentication completed
 }])
 
 .controller('QuestCtrl',['$scope', '$rootScope', '$state', 'Quests', function($scope, $rootScope, $state, Quests) {
