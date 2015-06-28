b76f3ef54250b89d9f1d4f06da694d27c946c869
Pranav Moktali <pmoktali@enova.com>
2015-06-28 06:26:30 -0500

    user authentication completed

--- www/js/backendServices.js
+++ www/js/backendServices.js
@@ -5,11 +5,59 @@ angular.module('starter.backendServices', [])
   return $firebaseArray(itemsRef);
 })
 
-.factory("Users", function($firebaseArray) {
-  var itemsRef = new Firebase("https://sizzling-fire-9071.firebaseio.com/users");
-  return $firebaseArray(itemsRef);
-});
 
+.factory("Users", ['$q', function($q){
+  var Users = {};
+  var d = $q.defer();
+  Users.createUser = function(username, password) {
+    var ref = new Firebase("https://sizzling-fire-9071.firebaseio.com");
+    ref.createUser({
+      email: username,
+      password: password
+    }, function(error, userData) {
+      if (error) {
+        d.reject(error)
+        switch (error.code) {
+          case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+          case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+          default:
+            console.log("Error creating user:", JSON.stringify(error));
+        }
+      } else {
+        d.resolve(userData);
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+    return d.promise();
+  };
+
+
+    Users.authenticateUser =function(username, password) {
+      var d = $q.defer();
+      var ref = new Firebase("https://sizzling-fire-9071.firebaseio.com");
+      ref.authWithPassword({
+        "email": username,
+        "password": password
+      }, function(error, authData) {
+        if (error) {
+          d.reject(error)
+          console.log("Login Failed!", JSON.stringify(error));
+        } else {
+          d.resolve(authData);
+          console.log("Authenticated successfully with payload:", JSON.stringify(authData));
+        }
+      });
+
+      return d.promise;
+    };
+
+    return Users;
+
+  }]);
 // .factory('Firebase', function(){
 //   var Firebase = {};
 //
